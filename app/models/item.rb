92718c9e6d331888ca94b4092b8e382ca948002b
Jhonnatan alexander Casas leon <JhonnatanCasasLeon@Jhonnatans-MacBook.local>
2019-09-14 04:18:11 +1000

    creation TDD models Todo and Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
+  # Association model
   belongs_to :todo
+  # validation
+  validates_presence_of :name
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  # Associtation test
+  # ensure an item record belongs to a single todo record
+  it { should belong_to(:todo)}
+  # Validation test
+  # ensure columns name is present before saving
+  it { should validate_presence_of(:name)}
 end
